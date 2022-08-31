3d7940f9081090f1a56d97f704378c066da59d4f
BRKittrell <109103574+BRKittrell@users.noreply.github.com>
2022-08-31 08:34:34 -0400

    Add files via upload

--- express_main.js
+++ express_main.js
@@ -1,36 +1,106 @@
-const { application } = require('express')
-const express = require('express')  //getting the express library
-const app = express()    //passing library into app
-const pets = require('./pets.json')
-const PORT = 8000
-//GET all
-app.get('/pets', function (req, res) {
-    res.status(200)
-    res.set('Content-Type', 'application/json')
-    res.send(JSON.stringify(pets))
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+let app = express();
+app.disable('x-powered-by');
+
+// READ
+app.get('/pets', (req, res) => {
+  console.log(req);
+  fs.readFile('pets.json', 'utf8', (err, petsData) => {
+    if (err) throw err;
+    let parsedPets = JSON.parse(petsData);
+    res.set('Content-Type', 'application/json');
+    res.send(parsedPets);
+  })
 })
-app.get('/pets/:id', function (req, res) {
-    const id = req.params.id
-    //if id is < 0, or id > pets.length-1 => log 404
-    //else
-    //if id is >= 0, && id < pets.length  => log 200
-    if (id < 0 || id > pets.length - 1) {
-        res.status(404)
-        res.set('Content-Type', 'text/plain')
-        res.send('Not Found')
-    } else if (id >= 0 && id < pets.length) {
-        res.status(200)
-        res.set('Content-Type', 'application/json')
-        res.send(pets[id])
+app.get('/pets/:id', (req, res) => {
+  fs.readFile('pets.json', 'utf8', (err, petsData) => {
+    let parsedPets = JSON.parse(petsData);
+    let id = req.params.id;
+    if (!isNaN(id) && id >= 0 && id < parsedPets.length) {
+      res.set('Content-Type', 'application/json');
+      res.send(JSON.stringify(parsedPets[id]));
+    } else {
+      res.set('Content-Type', 'text/plain');
+      res.sendStatus(404);
     }
+  })
 })
-app.post('/pets', function (req, res) {
-    if
-    res.status(200)
-    res.set('Content-Type', 'application/json')
-    res.send(pets)
+// CREATE
+app.post('/pets', (req, res) => {
+  fs.readFile('pets.json', 'utf8', (err, petsData) => {
+    let parsedPets = JSON.parse(petsData);
+    let newPet = req.body;
+    if (newPet.age && !isNaN(newPet.age) && newPet.kind && newPet.name) {
+      parsedPets.push(newPet);
+      fs.writeFile('pets.json', JSON.stringify(parsedPets), (writeErr) => {
+        if (writeErr) throw writeErr;
+        res.set('Content-Type', 'application/json');
+        res.send(newPet);
+      })
+    } else {
+      res.set('Content-Type', 'text/plain');
+      res.sendStatus(400);
+    }
+  })
 })
-app.listen(`${PORT}`, (req, res) => {
-    console.log(`Listening on port: ${PORT}`)
+// UPDATE
+app.patch('/pets/:id', (req, res) => {
+  fs.readFile('pets.json', 'utf8', (err, petsData) => {
+    let parsedPets = JSON.parse(petsData);
+    let updateObj = req.body;
+    let id = req.params.id;
+    if ((updateObj.age && !isNaN(updateObj.age)) || updateObj.name || updateObj.kind) {
+      if (parsedPets[id]) {
+        for (let prop in updateObj) {
+          if (prop === "age") updateObj[prop] = parseInt(updateObj[prop]);
+          parsedPets[id][prop] = updateObj[prop];
+        }
+        fs.writeFile('pets.json', JSON.stringify(parsedPets), (writeErr) => {
+          if (writeErr) throw writeErr;
+          res.set('Content-Type', 'application/json');
+          res.send(parsedPets[id])
+        })
+      } else {
+        res.set('Content-Type', 'text/plain');
+        res.sendStatus(404);
+      }
+    } else {
+      res.set('Content-Type', 'text/plain');
+      res.sendStatus(400);
+    }
+  })
 })
-module.exports = app
+// DELETE
+app.delete('/pets/:id', (req, res) => {
+  fs.readFile('pets.json', 'utf8', (err, petsData) => {
+    let parsedPets = JSON.parse(petsData);
+    let id = req.params.id;
+    if (parsedPets[id]) {
+      let pet = parsedPets[id];
+      parsedPets = parsedPets.splice(id, 1);
+      fs.writeFile('pets.json', JSON.stringify(parsedPets), (writeErr) => {
+        if (writeErr) throw writeErr;
+        res.set('Content-Type', 'application/json');
+        res.send(pet);
+      })
+    } else {
+      res.set('Content-Type', 'text/plain');
+      res.sendStatus(404);
+    }
+  })
+})
+// ERRORS
+app.get('/', (req, res) => {
+  res.set('Content-Type', 'text/plain');
+  res.sendStatus(404);
+})
+app.get('/:whatever', (req, res) => {
+  let whatever = req.params.whatever;
+  if (whatever !== "pets") {
+    res.set('Content-Type', 'text/plain');
+    res.sendStatus(404);
+  }
+})
+module.exports = app;

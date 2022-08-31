3ec5f0996b75d78c2342e88aa221477fda9a8a6b
BRKittrell <109103574+BRKittrell@users.noreply.github.com>
2022-08-31 08:35:56 -0400

    Add files via upload

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -2,41 +2,46 @@ const express = require('express');
 const app = express();
 const fs = require('fs')
 const PORT = 8001;
-
-app.use(express.json());
-
-app.post("./pets", function(req, res) {
-
-    console.log(req.body) //Able to access body from postman (must enter info in Postman using => body, raw, JSON) 
-
-    fs.readFile('./pets.json', "utf-8", (err,data) => {
+app.use(express.json());// post and patch
+// GET 1
+app.get('/pets/:id', (req, res) => {
+    const id = req.params.id
+    fs.readFile('./pets.json', 'utf-8', (err, data) => {
         if (err) throw err;
-        const parsedPets = JSON.parse(data)
-        parsedPets.push(req.body)
-        const stringifiedPets = JSON.stringify(parsedPets);
-        res.send(stringifiedPets);
-        console.log(stringifiedPets)
-        })
+        const parseData = JSON.parse(data)
+            const stringData = JSON.stringify(parseData[id])
+            res.status(200)
+            res.contentType('application/json')
+            res.send(stringData)
 
     const newPet = [{"name": "cornflake", "age": 3, "kind": "parakeet"}]
 
-    fs.writeFile('./pets.json', newPet, (err) => {
+   fs.writeFile('./pets.json', newPet, (err) => {
         if (err){
          throw err
         }
         res.status(200)
-        res.setHeader('Content-Type', 'application/json');
+        res.ContentType ('application/json');
         res.send(stringifiedPets)
     })
 })
+})
 
-app.get("./pets/3", function(req, res) {
+// CREATE 1
+// UPDATE 1
+// DELETE 1
+
+app.listen(`${PORT}`, () => {
+    console.log(`Listening on ${PORT}`)
+})
+
+app.get("/pets/3", function(req, res) {
     res.status(200)
-    res.setHeader('Content-Type', 'application/json');
+    res.Content-Type('application/json');
     res.send(req.body)
 })
 
-app.patch("./pets/3", function(req, res) {
+app.patch("/pets/3", function(req, res) {
     const petdata = require('./pets.json')
     const updatePet = {"name": "fido"}
     petdata.push(updatePet)
@@ -45,19 +50,19 @@ app.patch("./pets/3", function(req, res) {
     res.send(req.body)
 })
 
-app.get("./pets/3", function(req, res) {
+app.get("/pets/3", function(req, res) {
     res.status(200)
     res.setHeader('Content-Type', 'application/json');
     res.send(req.body)
 })
 
-app.delete("./pets/3", function(req, res) {
+app.delete("/pets/3", function(req, res) {
     res.status(200)
     res.setHeader('Content-Type', 'application/json');
     res.send(req.body)
 })
 
-app.get("./pets/3", function(req, res) {
+app.get("/pets/3", function(req, res) {
     res.status(404)
     res.setHeader('Content-Type, text/plain')
     res.send(req.body)
@@ -66,27 +71,3 @@ app.get("./pets/3", function(req, res) {
 app.listen(`${PORT}`, () => {
     console.log(`Listening on ${PORT}`)
 })
-
-
-////DO NOT MODIFY - ORIGINAL CONTENT
-// app.post("/pets", function(req, res) {
-
-//     console.log(req.body) //Able to access body from postman (must enter info in Postman using => body, raw, JSON) 
-
-//     fs.readFile('./pets.json', "utf-8", (err,data) => {
-//         if (err) throw err;
-//         const parsedPets = JSON.parse(data)
-//         parsedPets.push(req.body)
-//         const stringifiedPets = JSON.stringify(parsedPets);
-//         res.send(stringifiedPets);
-//         console.log(stringifiedPets)
-//         })
-
-//     fs.writeFile('./pets.json', (err) => {
-//         if (err) throw err;
-//         res.status(200)
-//         res.contentType('application/json')
-//         res.send(stringifiedPets)
-//     })
-// })
-
